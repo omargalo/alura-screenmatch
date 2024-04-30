22ae46932ed5067feff50be1890d695a4837636f
Omar García López <omar.garcia@omargl.net>
2024-04-30 09:42:36 -0600

    consumo API omdb

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package com.aluracursos.screenmatch;
 
+import com.aluracursos.screenmatch.service.ConsumoAPI;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,9 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hola desde Spring");
+		var consumoApi = new ConsumoAPI();
+		var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=better+call+saul&apikey=efd2e961");
+		//var json = consumoApi.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
 	}
 }
